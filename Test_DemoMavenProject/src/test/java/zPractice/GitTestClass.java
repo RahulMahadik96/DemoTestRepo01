91e3cc9f3f16734585465491552ad71a6e9aa059
Rahulmahadik39 <mahadikra39@gmail.com>
2024-05-03 19:39:20 +0530

    Git Test Class Created and comminting

---
+++ Test_DemoMavenProject/src/test/java/zPractice/GitTestClass.java
@@ -0,0 +1,14 @@
+package zPractice;
+
+import org.testng.annotations.Test;
+
+public class GitTestClass {
+	
+	
+	@Test
+	public void demoTest() {
+		
+		System.out.println("GitHub Testing TestMethod");
+	}
+
+}
